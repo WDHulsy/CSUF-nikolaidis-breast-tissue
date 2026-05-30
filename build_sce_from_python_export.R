5cb76a6983cdbf01d18694a3d49dc32ca123530d
WDHulsy <wdhulsy@csu.fullerton.edu>
2026-05-30 16:47:14 -0700

    Update build_sce_from_python_export.R

--- build_sce_from_python_export.R
+++ build_sce_from_python_export.R
@@ -1,41 +1,31 @@
 library(Matrix)
 library(SingleCellExperiment)
 
-setwd("C:/Users/drewh/Documents/DrNikolaidisMaterials/Lipidomics/deconvolution/reference")
+setwd("path/to/reference")
 
 IN <- "music_reference_export"
 
+
 counts <- readMM(file.path(IN, "counts_genes_by_cells.mtx"))
 
-genes <- read.delim(
-  file.path(IN, "genes.tsv"),
-  header = FALSE,
-  stringsAsFactors = FALSE
-)[[1]]
+genes <- read.delim(file.path(IN, "genes.tsv"), header=FALSE)[[1]]
 
-cells <- read.delim(
-  file.path(IN, "cells.tsv"),
-  header = FALSE,
-  stringsAsFactors = FALSE
-)[[1]]
+cells <- read.delim(file.path(IN, "cells.tsv"), header=FALSE)[[1]]
 
-meta <- read.delim(
-  file.path(IN, "cell_metadata.tsv"),
-  stringsAsFactors = FALSE
-)
+meta <- read.delim(file.path(IN, "cell_metadata.tsv"))
+
 
 rownames(counts) <- make.unique(genes)
+
 colnames(counts) <- cells
+
 rownames(meta) <- meta$cell_id
 
 meta <- meta[colnames(counts), ]
 
-sce <- SingleCellExperiment(
-  assays = list(counts = counts),
-  colData = meta
-)
+
+sce <- SingleCellExperiment(assays = list(counts = counts), colData = meta)
 
 saveRDS(sce, "breast_ref_sce.rds")
 
-print(sce)
-print(colnames(colData(sce)))
+print(colnames(colData(sce))) # verify cell_type and donor_id columns exist
